7a62cfbdb29775f965972566393cf1e92bb1b663
JOÃO PEDRO FERREIRA PONCHIROLI <joao_ponchiroli@estudante.sesisenai.org.br>
2024-08-01 21:06:49 -0300

    Adicionando o componente Title ao projeto

--- Front-end/registroPessoaFisica/src/App.jsx
+++ Front-end/registroPessoaFisica/src/App.jsx
@@ -1,5 +1,13 @@
 import Form from '../src/components/Form'
+import Title from '../src/components/Title'
 
 export default function App() {
-  return <Form/ >;
+  return (
+  <>
+    <Title style={{ display: "flex", justifyContent: "center"}}>
+      Registro Pessoa Física
+    </Title>
+    <Form/ >
+    </>
+  )
 }
---
+++ Front-end/registroPessoaFisica/src/components/Title/index.jsx
@@ -0,0 +1,5 @@
+export default function Title({children, style}){
+    return(
+        <h1 style={style}>{children}</h1>
+    )
+}
